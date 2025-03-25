b16d7bf6c0bcbe5e365e27432717a127f6725b2e
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-03-25 17:51:35 +0900

    [level 0] Title: 양꼬치, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,17 @@
+using System;
+
+ public class Solution
+ {
+     public int solution(int n, int k)
+     {
+         int answer = 0;
+
+         answer = 12000 * n + 2000 * k;
+         
+         if (n >= 10)
+         {
+             return answer - n/10*2000;
+         }
+         return answer;
+     }
+ }
