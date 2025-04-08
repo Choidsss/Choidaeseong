e08e8d9fdf4d31eee5a2f7a419c7757a5da5d498
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-04-08 17:25:56 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.28 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+    public class Solution
+   {
+       public int solution(int[] dot)
+       {
+           int answer = 0;
+
+           answer = dot[0] > 0 && dot[1] > 0 ? 1 : dot[0] < 0 && dot[1] > 0 ? 2
+                   : dot[0] < 0 && dot[1] < 0 ? 3 : 4;
+           return answer;
+       }
+   }
