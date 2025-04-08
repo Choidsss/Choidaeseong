b776a0b609a1d7b3c0ca8fa9e71b5a286cc10953
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-04-08 17:26:04 +0900

    [level 0] Title: 모음 제거, Time: 0.55 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution
+{
+    public string solution(string my_string)
+    {
+        string answer = "";
+
+        my_string = my_string.Replace("a", "");
+        my_string = my_string.Replace("e", "");
+        my_string = my_string.Replace("i", "");
+        my_string = my_string.Replace("o", "");
+        my_string = my_string.Replace("u", "");
+
+        return my_string;
+    }
+}
