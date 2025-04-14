d7f08d944dd514b76a288e63924f05a9488f5e97
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-04-14 19:04:33 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 2.03 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] numbers)
+    {
+        int answer=0;
+
+        Array.Sort(numbers);
+
+        answer = numbers[numbers.Length - 1] * numbers[numbers.Length-2];
+        if (answer < numbers[0] * numbers[1])
+        {
+            answer = numbers[0] * numbers[1];
+        }
+        return answer;
+    }
+}
