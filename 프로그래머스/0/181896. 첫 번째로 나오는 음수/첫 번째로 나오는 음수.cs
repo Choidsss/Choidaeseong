19271ba880e0739a35471367a8e861850afab155
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-04-23 18:36:02 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.52 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] num_list)
+    {
+        int answer = 0;
+
+        for (int i=0;i<num_list.Length;i++)
+        {
+            if (num_list[i]<0)
+            {
+                answer = Array.IndexOf(num_list, num_list[i]);
+                break;
+            }
+            else
+            {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
