90087feb823ddde3e4c80ad47cea262f0e18935a
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-04-28 17:36:13 +0900

    [level 0] Title: 카운트 다운, Time: 0.28 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.cs
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.cs
@@ -6,14 +6,7 @@ public class Solution
     {
         int index = 0;
         int count=0;
-
-
-        for (int i = start_num; i >= end_num; i--)
-        {
-            count++;
-        }
-
-        int[] answer = new int[count];
+        int[] answer = new int[start_num-end_num+1];
 
         for (int i=start_num;i>=end_num;i--)
         {
