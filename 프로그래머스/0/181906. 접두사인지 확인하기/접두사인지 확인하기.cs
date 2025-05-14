f7da68beb175e488276a645283df957d515814f9
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-05-14 17:55:10 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 4.87 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cs
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cs
@@ -5,9 +5,7 @@ public class Solution
     public int solution(string my_string, string is_prefix)
     {
         int answer = 0;
-
-        for (int i = 0; i < my_string.Length; i++)
-        {
+        
             if (my_string.StartsWith(is_prefix)==true)
             {
                 answer = 1;
@@ -16,7 +14,7 @@ public class Solution
             {
                 answer = 0;
             }
-        }
+        
         return answer;
     }
 }
