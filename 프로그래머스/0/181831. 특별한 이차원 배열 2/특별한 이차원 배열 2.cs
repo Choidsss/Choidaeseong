9dd8d3ddd5888a3b072dc1bfa2dbcc4e5f15e84b
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-05-19 18:40:13 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.80 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[,] arr)
+    {
+        int answer = 0;
+        int count = 0;
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                //???
+                if (arr[i, j] == arr[j, i])
+                {
+                    count++;
+                }
+                else break;
+            }
+        }
+
+        if (count == arr.Length)
+        {
+            answer = 1;
+        }
+        else answer = 0;
+
+        return answer;
+    }
+}
