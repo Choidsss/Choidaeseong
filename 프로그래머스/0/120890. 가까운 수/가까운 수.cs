799eb9a96d86792eff2900465cb6eedbfd3f32ed
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-07-22 16:54:04 +0900

    [level 0] Title: 가까운 수, Time: 4.05 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.cs
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.cs
@@ -1,27 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Solution
 {
     public int solution(int[] array, int n)
-{
-    int answer = array[0];
-    int num = Math.Abs(array[0] - n);
-
-    for (int i = 0; i < array.Length; i++)
     {
-        int compareNum = Math.Abs(array[i] - n);
+        int answer = array.OrderBy((x) => x).FirstOrDefault((x) => Math.Abs(x - n) == array.Min((o) => Math.Abs(o - n)));
 
-        if (compareNum < num)
-        {
-            num = compareNum;
-            answer = array[i];
-        }
-        else if (compareNum == num && array[i] < answer)
-        {
-            answer = array[i];
-        }
+        return answer;
     }
-
-    return answer;
-}
 }
