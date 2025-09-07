ce98d86d12be5b08317b76a443e74fb69292338a
Choidsss <133637761+Choidsss@users.noreply.github.com>
2025-09-07 17:17:09 +0900

    [level 0] Title: 공 던지기, Time: 0.27 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] numbers, int k)
+    {
+        int answer = 0;
+
+        for (int i=0;i<k-1;i++)
+        {
+            answer += 2;
+
+            if (answer>numbers.Length)
+            {
+                answer = answer % numbers.Length;
+            }
+        }
+        return numbers[answer];
+    }
+}
