3b8a183abc0d8c443d0e8a1d54c990c15b2395dd
Choidsss <133637761+Choidsss@users.noreply.github.com>
2026-03-14 16:43:34 +0900

    [level 1] Title: 기사단원의 무기, Time: 7233.76 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int solution(int number, int limit, int power)
+    {
+        int answer = 0;
+        
+        for (int i = 1;i<=number;i++)
+        {
+            int n = 0;
+
+            for (int j = 1;j<=i/2;j++)
+            {
+                if(i % j == 0)
+                {
+                    n++;
+                }
+            }
+
+            n++;
+
+            if (n <= limit)
+            {
+                answer += n;
+            }
+            else
+            {
+                answer += power;
+            }
+        }
+        return answer;
+    }
+}
